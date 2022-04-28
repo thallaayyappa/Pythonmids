84998150bb448057b6fb660b8efdcec814d98c7c
ayyappa <thalla.ayyappa@gmail.com>
2022-04-28 12:06:30 -0400

    S000000-added controlflows

---
+++ Controlflow.py
@@ -0,0 +1,24 @@
+#Arithmetic, relational, logical
+
+# contorl statment,
+#conditional
+ #   if  if..else elif
+ #looping Statement
+  #  while for
+
+ #jumping statment
+#break continue
+
+
+            # name,age,sal="john",30,5000
+# #print(name, age, sal)
+# #approach 2
+# print("name is:%s age is:%d, sal as:%g" %(name, age, sal))
+
+num1= (input("Enter number:"))
+num2= (input("enter second numer:"))
+print(float(num1)+float(num2))
+
+#casting
+
+
